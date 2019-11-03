690c91219f5139e31b9cb975fc583077136036f4
MGTHEBOSS <a46105@gmail.com>
2019-11-03 19:09:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+    response.sendFile(__dirname + "/views/index.html");
 });
 
 // listen for requests :)
 const listener = app.listen(8080, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
